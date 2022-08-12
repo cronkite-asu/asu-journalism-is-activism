50c626f6620dbf0735859ba7ff66cadc93a79850
Devin Blankenship <devin@fervorcreative.com>
2022-08-11 17:57:51 -0700

    added more images, copy, and css to roughly 3 pages.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// var WaveSurfer = require("wavesurfer.js");
+// import WaveSurfer from "wavesurfer.js";
+
 $(document).ready(function(){
   $('.fade').slick({
   dots: false,
@@ -7,5 +10,22 @@ $(document).ready(function(){
   autoplaySpeed: 4000,
   });
 });
+
+// audioPlayer = function() {
+
+//   var wavesurfer = WaveSurfer.create({
+//     container: '#waveform'
+//   });
+
+//   wavesurfer.load('../audio/Beyond the Reporting.mp3');
+
+//   wavesurfer.getDuration('../audio/Beyond the Reporting.mp3');
+
+//   wavesurfer.on('ready', function () {
+//     wavesurfer.play('../audio/Beyond the Reporting.mp3');
+//   });
+
+// }
+// audioPlayer();
 
 
